1897e6abc9cb321b762939b36085bbcdc7da617a
Ariful Islam Akash <ariful.akash23@gmail.com>
2024-12-12 12:32:58 +0600

    Our 1st push

---
+++ HSJProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HSJProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
