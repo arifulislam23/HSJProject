5af44f5d7c3521fa78d0a8cee87654f30e19cd6a
Ariful Islam Akash <ariful.akash23@gmail.com>
2024-12-17 14:10:44 +0600

    admin layout

---
+++ HSJProject/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HSJProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
